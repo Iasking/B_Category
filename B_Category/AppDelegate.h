57630f1860150e77012a8a6815a6fae1dc2f0823
jinpeng <jinpeng@chezhibao.com>
2018-12-04 15:58:48 +0800

    Initial Commit

---
+++ B_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  B_Category
+//
+//  Created by Parker on 2018/12/4.
+//  Copyright © 2018年 Parker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
