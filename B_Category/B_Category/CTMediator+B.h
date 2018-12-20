1c875b07183e80615bbb42b5b9f88a647fc76695
1012948435@qq.com <jinpeng@chezhibao.com>
2018-12-20 16:17:47 +0800

    首次提交

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by Parker on 2018/12/4.
+//  Copyright © 2018年 Parker. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+@interface CTMediator (B)
+
+@end
---
+++ B_Category/B_Category/CTMediator+B.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+B.m
+//  B_Category
+//
+//  Created by Parker on 2018/12/4.
+//  Copyright © 2018年 Parker. All rights reserved.
+//
+
+#import "CTMediator+B.h"
+
+@implementation CTMediator (B)
+
+@end
