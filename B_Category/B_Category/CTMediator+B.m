a42f592646f6726d6367542de4fab143bcb82f07
1012948435@qq.com <jinpeng@chezhibao.com>
2018-12-20 16:18:15 +0800

    第一次完整提交

--- B_Category/B_Category/CTMediator+B.m
+++ B_Category/B_Category/CTMediator+B.m
@@ -10,4 +10,13 @@
 
 @implementation CTMediator (B)
 
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText
+{
+    /*
+     BViewController *viewController = [[BViewController alloc] initWithContentText:@"hello, world!"];
+     */
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    params[@"contentText"] = contentText;
+    return [self performTarget:@"B" action:@"viewController" params:params shouldCacheTarget:NO];
+}
 @end
